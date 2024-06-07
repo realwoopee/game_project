ad9a1bb72144e01b97005d0efd9b957ba821d674
Cherry on a cake <ninjaxxxflame@gmail.com>
2024-06-07 12:19:08 +0700

    HealthBar implemented. Console thrown NullReferenceExceptions each time health changes yet everything works as intended. You can find HealthBar in Playground -> UI -> HealthBar.

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -252,6 +252,8 @@ namespace StarterAssets
             // normalise input direction
             Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
 
+            
+
             // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
             // if there is a move input rotate player when the player is moving
             if (_input.move != Vector2.zero)
